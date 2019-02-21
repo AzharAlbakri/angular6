23bbd84c30e6c5a58b0acb397dd609eec4346d22
AzharAlbakri <albakriazhar.work@gmail.com>
2019-02-21 14:40:18 +0200

    sign up page design

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,12 @@ import { LogInComponent } from './components/pages/log-in/log-in.component';
 import { HomeComponent } from './components/pages/home/home.component'
 import { NewsComponent } from './components/pages/news/news.component';
 import { MyProfileComponent } from './components/pages/my-profile/my-profile.component';
+import { SignUpComponent } from './components/pages/sign-up/sign-up.component';
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
 
-  {path: 'sign-up', component: LogInComponent},
+  {path: 'sign-up', component: SignUpComponent},
   {path: 'sign-in', component: LogInComponent},
   {path: 'news', component: NewsComponent},
   {path: 'my-profile', component: MyProfileComponent}
