074f3bb03e0ceeaab62cd10faee44de9f9d1cdc1
AmjadHasnawi <civilamjad227@gmail.com>
2019-02-21 20:19:39 +0200

    try to deploy

--- server.js
+++ server.js
@@ -164,12 +164,12 @@ app.get('/modifyUser', (req, res) => {
   });
 })
 
-// if (process.env.NODE_ENV === "production") {
+if (process.env.NODE_ENV === "production") {
   // Serve any static files
   app.use(express.static(path.join(__dirname, 'dist/newsApp')));
   // Handle React routing, return all requests to React app
   app.get("*", function(req, res) {
     res.sendFile(path.join(__dirname, 'dist/newsApp/index.html'));
   });
-// }
+}
 app.listen(port, () => console.log(`Listening on port ${port}`));
